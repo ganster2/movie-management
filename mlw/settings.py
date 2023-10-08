f24db02a87286bf068f4fabb7fb77b7fe91edd27
ganster2 <nurmuhammad200509@gmail.com>
2023-10-07 23:03:44 -0700

    asas
    sqsqsasd

--- mlw/settings.py
+++ mlw/settings.py
@@ -35,7 +35,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = ['*']
 
@@ -49,6 +49,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'static',
+    'staticfiles',
+    'movie-management',
+    'media',
 
     'movies',
     'users',
